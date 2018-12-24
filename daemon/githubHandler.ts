d29eb2cd0c55e683b07d13be4963ccc17b3650c4
Cameron Tacklind <cameron@tacklind.com>
2018-12-24 01:40:41 -0800

    Authenticate

--- daemon/githubHandler.ts
+++ daemon/githubHandler.ts
@@ -3,6 +3,8 @@ import * as Octokit from '@octokit/rest';
 const octokit = new Octokit();
 
 export default function github(token: string) {
+  octokit.authenticate({ type: 'token', token });
+
   async function getRepositoryList() {
     octokit;
   }
