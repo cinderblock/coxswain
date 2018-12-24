b1323473672b2ca0486ea6bb54e96e61c98fc20e
Cameron Tacklind <cameron@tacklind.com>
2018-12-24 02:03:39 -0800

    List repositories

--- ui/Coxswain.tsx
+++ ui/Coxswain.tsx
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import SocketConnection, { eventHandler, notify } from './SocketConnection';
+
+import { Repository } from '../daemon/githubHandler';
+
+function repository(repo: Repository, index: number) {
+  return (
+    <li
+      key={index}
+    >
+      {repo.full_name}
+    </li>
+  );
+}
 
 export default function Coxswain(props: any) {
-  return <>Authed</>;
+  const [repolist, setRepolist] = useState<Repository[]>([]);
+
+  useEffect(notify('repositories', setRepolist));
+
+  return <ul>{repolist && repolist.map(repository)}</ul>;
 }
