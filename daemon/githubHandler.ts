457293f684de9361608068918c7fcc783e7bf63b
Cameron Tacklind <cameron@tacklind.com>
2018-12-24 01:41:13 -0800

    Actually load full list of repos from github

--- daemon/githubHandler.ts
+++ daemon/githubHandler.ts
@@ -2,11 +2,32 @@ import * as Octokit from '@octokit/rest';
 
 const octokit = new Octokit();
 
+type Repository = Octokit.AppsListReposResponseRepositoriesItem;
+
 export default function github(token: string) {
   octokit.authenticate({ type: 'token', token });
 
   async function getRepositoryList() {
-    octokit;
+    const list: Repository[] = [];
+    let i = 1;
+    // max of 100 per API
+    const per_page = 100;
+    const maxPages = 100;
+    while (i < maxPages) {
+      const oldLength = list.length;
+      const next = (await octokit.repos.list({ per_page, page: i++ })).data;
+      // list = list.concat(next);
+      list.splice(-1, 0, ...next);
+
+      // Either of the following checks should be sufficient.
+
+      // If we haven't changed length
+      if (list.length == oldLength) break;
+
+      // If we got fewer than then max length
+      if (next.length < per_page) break;
+    }
+    return list;
   }
 
   return {
