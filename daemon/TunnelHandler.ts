b5c1c439b5eec569f6dfd92d7be6596939d1e1fd
Cameron Tacklind <cameron@tacklind.com>
2018-12-25 01:24:50 -0800

    Generate a UUID for the tunnel

--- daemon/TunnelHandler.ts
+++ daemon/TunnelHandler.ts
@@ -1,11 +1,13 @@
 import ngrok from 'ngrok';
+import uuid from 'uuid/v4';
 
 export default function(port: number) {
   const URL = ngrok.connect(port);
+  const id = uuid();
 
   function url() {
     return URL;
   }
 
-  return { url };
+  return { url, id };
 }
